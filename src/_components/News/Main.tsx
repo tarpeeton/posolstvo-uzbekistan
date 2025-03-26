4ca3fc66121e8da51ce02693df9e9d3e72adf1db
tarpeeton <tarpeetonteam@gmail.com>
2025-03-26 14:53:22 +0500

    tailwind dark mode off

--- src/_components/News/Main.tsx
+++ src/_components/News/Main.tsx
@@ -5,7 +5,7 @@ import { LatestsNews } from "./Latests"
 
 export const MainNews = () => {
     return (
-        <main className="px-[20px] lg:px-[120px]">
+        <main className="px-[20px] lg:px-[120px] " >
             <LatestsNews />
         </main>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
+    darkMode: false,
     content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/*.{js,ts,jsx,tsx,mdx}",
