ace795c4c91e01adf922c78473cbfa3dbbe92a9a
tarpeeton <tarpeetonteam@gmail.com>
2025-03-28 18:12:18 +0500

    history

---
+++ src/_components/Embassy/MissionHistory/Main.tsx
@@ -0,0 +1,62 @@
+import { Breadcrumb } from "@/ui/breadcrumb";
+import { useLocale, useTranslations } from "next-intl";
+import { HISTORY_MISSION } from "@/constants/history-mission";
+import { GoDownload } from "react-icons/go";
+
+
+export const MissionHistoryMain = () => {
+  const t = useTranslations();
+  const locale = useLocale();
+
+  return (
+    <section className="px-[20px] lg:px-[120px] pb-[80px] lg:pb-[120px]">
+      <div className="mt-[12px] lg:mt-[24px]">
+        <Breadcrumb
+          items={[
+            {
+              label: {
+                ru: "Главная",
+                uz: "Bosh sahifa",
+                en: "Home",
+              },
+              href: "/",
+            },
+            {
+              label: {
+                ru: "Посольство",
+                uz: "Elchixona",
+                en: "Embassy",
+              },
+              href: "/embassy",
+            },
+            {
+              label: {
+                ru: "История миссии",
+                uz: "",
+                en: "",
+              },
+              isCurrent: true,
+            },
+          ]}
+        />
+      </div>
+
+      <div className="mt-[24px] lg:mt-[48px]">
+        <h1 className="mb-[26px] lg:mb-[30px] text-[20px] lg:text-[32px]">
+          {t("mission_history")}
+        </h1>
+        <div className="flex flex-col gap-[16px] lg:gap-[20px]">
+          <p className="text-[16px] lg:text-[20px] font-medium">
+            {HISTORY_MISSION.title[locale]} 
+          </p>
+          <p className="text-[16px]">{HISTORY_MISSION.description[locale]}</p>
+
+          <a   href="/documents/mission-history.pdf" download className="flex flex-row   justify-center cursor-pointer px-[10px] h-[40px] lg:w-[290px] gap-2 items-center lg:h-[40px] rounded-[4px] bg-[#427EFF] lg:px-[16px] text-white">
+            {t("historical_documents")}
+            <GoDownload  className="w-[18px] h-[18px]"/>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/app/[locale]/embassy/mission-history/page.tsx
@@ -0,0 +1,10 @@
+import { MissionHistoryMain } from "@/_components/Embassy/MissionHistory/Main";
+
+
+export default function EmbassyMainPage() {
+  return (
+    <main>
+      <MissionHistoryMain />
+    </main>
+  );
+}
