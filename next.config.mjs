2d6cf152789a337c3d12cc710654b8b0ae2d1fa3
tarpeeton <tarpeetonteam@gmail.com>
2025-06-09 20:39:55 +0500

    fix(Image Domain)

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,19 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    sassOptions: {
-        implementation: 'sass-embedded',
-      },
-    images: {
-        domains: ['ucarecdn.com',   'images.unsplash.com' , 'via.placeholder.com' , "95.182.118.106"],
-    },
+  sassOptions: {
+    implementation: "sass-embedded",
+  },
+  images: {
+    domains: [
+      "ucarecdn.com",
+      "images.unsplash.com",
+      "via.placeholder.com",
+      "95.182.118.106",
+      "api.pakistan.money-manager.uz",
+      "api.pakistan.money-manager.uz",
+    ],
+  },
 };
 
 export default withNextVideo(withNextIntl(nextConfig));
